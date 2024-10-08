ef7d6f2737ef56d4e8d3c0f7b99135c03b578499
HuynhHan185 <huynhgiahantn@gmail.com>
2024-10-08 23:48:35 +0700

    Delete src/src directory

--- src/src/tuan6GiaoDich/InterfaceGD.java
+++
@@ -1,8 +0,0 @@
-package tuan6GiaoDich;
-
-public interface InterfaceGD {
-	double tinhThanhTien(); // Tính thành tiền của giao dịch
-    String getMaGiaoDich(); // Lấy mã giao dịch
-    double getDonGia(); // Lấy đơn giá
-    int getSoLuong(); // Lấy số lượng
-}
